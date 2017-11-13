67acde59d0132d6169a286c895d3ad69339381a2
aldonalis <32141065+aldonalis@users.noreply.github.com>
2017-11-13 19:35:45 +0000

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './App.css';
+import Playlist from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: '', artist: '', album: ''},
+        {name: '', artist: '', album: ''},
+        {name: '', artist: '', album: ''},
+      ],
+      playlistName: 'Jammming Playlist',
+      playlistTracks: [
+        {name: '', artist: '', album: ''},
+        {name: '', artist: '', album: ''},
+        {name: '', artist: '', album: ''}
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    //this.updatePlaylistName = this.updatePlaylistName.bind(this);
+  }
+
+
+addTrack(track) {
+  let tracks = this.state.playListTracks;
+  if (!tracks.includes(track.id)) {
+    tracks.push(track.id);
+    }
+  this.setState({
+    playListTracks: tracks
+  })
+}
+
+removeTrack(track) {
+    let tracks = this.state.playListTracks;
+    if (tracks.includes(track.id)) {
+      let position = tracks.indexOf(track.id);
+      tracks.splice(position, 1);
+    }
+    this.setState({
+      playListTracks: tracks
+    })
+}
+
+/*updatePlaylistName(name) {
+  this.setState({
+    playlistName: name
+  })
+}*/
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              //onNameChange={this.updatePlaylistName}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
